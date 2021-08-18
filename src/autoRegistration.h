c5cdb2bbf5a42d2e12d6758d08409e37dff7410b
pmdifran <pmdifran@gmail.com>
2021-08-17 21:26:52 -0400

    commit for code review PR

--- src/autoRegistration.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "GeoDetection.h"
-#include <vector>
-
-Eigen::Matrix4f globalRegistration(GeoDetection::Cloud& reference, 
-	GeoDetection::Cloud& source, const float& radius = 1.0, const float& subres = 0.15);
-
-Eigen::Matrix4f icpRegistration(GeoDetection::Cloud& reference, 
-	GeoDetection::Cloud& source, const float radius = 1);
