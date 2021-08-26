1518ec29e4b01b55916b76b3cc7c5de6091f8f69
pmdifran <pmdifran@gmail.com>
2021-08-25 20:55:42 -0400

    Changed to wrapper around std::vector<float>, allocated on the stack.
    Operators overloaded so that access is similar to std::vector.

--- src/ScalarField.h
+++ src/ScalarField.h
@@ -3,27 +3,63 @@
 
 namespace GeoDetection
 {
-	class ScalarField : public std::vector<float>
+	//Scalar field class (wrapper of std::vector<float> with name)
+	class ScalarField
 	{
-	private:
-		const char* m_name = "Default";
+	public:
+		const char* name = "Default";
+		std::vector<float> data;
 
 	public:
-		using Ptr = std::shared_ptr<ScalarField>;
-
+		//Constructors
 		ScalarField() = default;
-		ScalarField(const char* name) : m_name(name) {}
 
+		ScalarField(int64_t size) :
+			data(std::vector<float>(size)) {}
+
+		ScalarField(int64_t size, const char* name) : 
+			data(std::vector<float>(size)),
+			name(name) {}
+
+		ScalarField(int64_t size, float init_value, const char* name) :
+			data(std::vector<float>(size, init_value)),
+			name(name) {}
+
+		ScalarField(std::vector<float>& vec) :
+			data(vec) {}
+
+		ScalarField(std::vector<float>&& vec) :
+			data(std::move(vec)) {}
+
+		//Copy & Move Ctors
 		ScalarField(const ScalarField& scalar_field) = default;
 		ScalarField(ScalarField&& scalar_field) = default;
 
+		//Assignment Operators
 		ScalarField& operator=(const ScalarField&) = default;
-		ScalarField& operator=(ScalarField&&) = default;
+		ScalarField& operator=(ScalarField&& rhs) = default;
 
-		inline void setName(const char* name) { m_name = name; }
-		inline const char* const getName() const { return m_name; }
+		ScalarField& operator=(std::vector<float>& vec)
+		{
+			data = vec;
+			return *this;
+		}
 
+		ScalarField& operator=(std::vector<float>&& vec)
+		{
+			data = std::move(vec);
+			return *this;
+		}
 
-		
+		//Vector Access Operators
+		float& operator[](const size_t index)
+		{
+			return data[index];
+		}
+
+	public:
+		size_t size() { return data.size(); }
+		std::vector<float>::iterator begin() { return data.begin(); }
+		std::vector<float>::iterator end() { return data.end(); }
 	};
 }
